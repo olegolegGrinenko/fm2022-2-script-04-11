faf626ee40712cc62ebf71fec844c7dae8669362
OlegFC <grinenkofreshcode@gmail.com>
2022-11-06 12:28:08 +0200

    Use types & create let variables

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-alert (505)
+console.log(505)
+console.log('string')
+console.log(true)
+console.log(null)
+console.log(undefined)
+
+console.log(5+12)
+
+ let userFirstname;
+ userFirstname = 'Oleg';
+ userFirstname = 'Brad';
+ console.log(userFirstname);
+
+ let isSunInSkyNow = false;
+  
